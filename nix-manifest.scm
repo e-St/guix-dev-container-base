1b3eac8bb5d5ff7b8f691513fca74f672fa2063a
e-St <10636497+e-St@users.noreply.github.com>
2024-10-22 20:52:49 +0200

    Update nix-manifest.scm

--- nix-manifest.scm
+++ nix-manifest.scm
@@ -1,4 +1,4 @@
-(use-modules (gnu) (guix utils) (guix packages))
+(use-modules (gnu) (guix packages))
 
 ;; Define variables at the top for easy management
 (define shell-file "./shell.nix") ; Path to the shell.nix file
@@ -6,16 +6,12 @@
 ;; Specify the Nix package
 (define nix-package (specification->package "nix"))
 
-;; Define the manifest
+;; Define the manifest for the container
 (specifications
  (list nix-package))
 
-;; Custom command to run nix-shell automatically
+;; Note: The command will be executed in the context of the container
 (define container-command
   (string-append "bash -c \"nix-shell " shell-file "\""))
 
-;; Use the container command
-(operating-system
-  (name "my-container")
-  (services %base-services)
-  (command container-command))
+;; Use this command when running the container
